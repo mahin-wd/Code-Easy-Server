dd0ccd3e400fcfc4837fab15ceeee441440c7e1a
mahin-wd <wdmahin.mhm@gmail.com>
2022-10-26 08:43:22 +0600

    server api created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 4000;
+
+
+app.use(cors());
+
+const courses = require('./data/courses.json')
+
+app.get('/', (req, res) => {
+    res.send('Courses API Running');
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c.id === id)
+    res.send(selectedCourse);
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port', port);
+})
